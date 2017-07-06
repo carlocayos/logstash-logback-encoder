1eb9230a63c64ada7c9ccbe3b24033e97a8e355c
Carlo.Cayos <carlo.cayos@intelematics.com>
2017-07-06 10:50:37 +1000

    Initial commit

---
+++ src/main/java/com/carlocayos/LogstashLogbackEncoderApplication.java
@@ -0,0 +1,12 @@
+package com.carlocayos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogstashLogbackEncoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogstashLogbackEncoderApplication.class, args);
+	}
+}
